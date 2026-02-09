38fd4a7f8805024b89dd51552cd6bb979c9426b2
BeZZuBik41 <33658461+BeZZuBik41@users.noreply.github.com>
2026-02-09 20:22:23 +0700

    Убираем мусорный комментарий

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -3,7 +3,6 @@ export * from './contracts/websocket/websocket-events-server-to-client.contract'
 export * from './contracts/websocket/websocket-events-client-to-server.contract';
 
 // RabbitMQ contracts (message broker events)
-// Chat events moved to lib/src/chats/ (single source of truth)
 export * from './contracts/rabbitmq/user-events.contract';
 
 // Types & interfaces
