711de00d15367e71cdaf175ca00d2949cb09aa8c
BeZZuBik41 <33658461+BeZZuBik41@users.noreply.github.com>
2026-03-05 21:20:33 +0700

    Обновление контрактов для регистрации и создание пользователя

--- src/profiles/contracts/events/profile-user-created.event.ts
+++ src/profiles/contracts/events/profile-user-created.event.ts
@@ -3,9 +3,10 @@ import { UserRole } from '../../../types/user-role.enum';
 
 export interface ProfileUserCreatedData {
     user_id: string;
-    account_id: string;
     role_slug: UserRole;
     organization_id: string | null;
+    email: string;
+    phone: string;
 }
 
 export class ProfileUserCreatedEventContract implements IBaseEventContract<ProfileUserCreatedData> {
